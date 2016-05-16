f862d4fd62534dd8b19ee992236fcee754fef517
cassiodeveloper <cassio@cassiobp.com.br>
2016-05-16 16:49:27 -0300

    Remove comments

--- map.js
+++ map.js
@@ -19,15 +19,14 @@ function initMap() {
 
 function setMarkers() {
     $.getJSON("dados.json", function(json) {
-        console.log(json);
-        // $.each(json.bicicletarias, function(key, data) {
-        //     var latLng = new google.maps.LatLng(data.lat, data.lng); 
-        //     // Creating a marker and putting it on the map
-        //     var marker = new google.maps.Marker({
-        //         position: latLng,
-        //         map: map,
-        //         title: data.nome
-        //     });
-        // });
+        $.each(json.bicicletarias, function(key, data) {
+            var latLng = new google.maps.LatLng(data.lat, data.lng); 
+            // Creating a marker and putting it on the map
+            var marker = new google.maps.Marker({
+                position: latLng,
+                map: map,
+                title: data.nome
+            });
+        });
     });    
 };
