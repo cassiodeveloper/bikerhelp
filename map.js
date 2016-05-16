dc205016fcb316b65769f14e40ba657713e81893
cassiodeveloper <cassio@cassiobp.com.br>
2016-05-16 17:05:42 -0300

    Agora vai!

--- map.js
+++ map.js
@@ -12,14 +12,13 @@ function initMap() {
     
     var map = new google.maps.Map(document.getElementById("map"), mapOptions);
     
-    //setMarkers();
+    setMarkers();
 }
 
 function setMarkers() {
     $.getJSON("dados.json", function(json) {
         $.each(json.bicicletarias, function(key, data) {
             var latLng = new google.maps.LatLng(data.lat, data.lng); 
-            // Creating a marker and putting it on the map
             var marker = new google.maps.Marker({
                 position: latLng,
                 map: map,
